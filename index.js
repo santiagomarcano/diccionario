43c306dbf4e12bbaf109ef9f6f00f8761448be1b
Santiago Marcano <marcanoblog@gmail.com>
2021-11-24 10:01:33 +0100

    FIRE

---
+++ index.js
@@ -0,0 +1,8 @@
+// const { saludo, sumar } = require("./test");
+
+// const suma = sumar(10, 10);
+
+// console.log(saludo);
+
+
+console.log(document)
---
+++ test.js
@@ -0,0 +1,10 @@
+const saludo = "Hola Mundo!"
+
+function sumar(a, b) {
+    return a + b;
+}
+
+module.exports = {
+    saludo,
+    sumar
+}
